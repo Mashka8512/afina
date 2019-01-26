265ee2c8dca6b268d94079834b7190747e93737e
MatyushinMA <Itsaprank@yandex.ru>
2019-01-26 12:51:53 +0300

    (FP) ClientBuffer fix

--- src/network/st_nonblocking/ClientBuffer.h
+++ src/network/st_nonblocking/ClientBuffer.h
@@ -23,7 +23,7 @@ public:
     void conditional_reset();
 
 private:
-    static std::size_t _size = 4096;
+    static std::size_t _size;
     char buffer[_size];
     std::size_t read_offset = 0;
     std::size_t parsed_offset = 0;
